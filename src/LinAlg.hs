8f3e457d55dd1e298faa44469c9a8430c318baa0
Conal Elliott <conal@conal.net>
2020-07-24 18:11:37 -0700

    Scale experiment to eliminate Par1 use

--- src/LinAlg.hs
+++ src/LinAlg.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-} -- see below
 
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
 -- | Linear algebra after Fortran
 
 module LinAlg where
@@ -14,9 +16,6 @@ import Data.Functor.Rep
 
 infixl 7 :*
 type (:*)  = (,)
-
--- infixl 6 :+
--- type (:+)  = Either
 
 unzip :: Functor f => f (a :* b) -> f a :* f b
 unzip ps = (fst <$> ps, snd <$> ps)
@@ -52,17 +51,18 @@ infixr 2 :|#
 -- | Compositional Linear map representation. @L f g s@ denotes @f s -* g s@,
 -- where @(-*)@ means linear functions.
 data L :: (* -> *) -> (* -> *) -> (* -> *) where
-  Zero :: L f g s
-  Scale :: s -> L Par1 Par1 s
+  Zero  :: L f g s
+  Scale :: s -> L f f s
   (:|#) :: L f h s -> L g h s -> L (f :*: g) h s
   (:&#) :: L f h s -> L f k s -> L f (h :*: k) s
   JoinL :: V h => h (L f g s) -> L (h :.: f) g s
   ForkL :: V h => h (L f g s) -> L f (h :.: g) s
 
 unjoin2 :: Additive s => L (f :*: g) h s -> L f h s :* L g h s
-unjoin2 Zero = (zero,zero)
+unjoin2 Zero = (Zero,Zero)
+unjoin2 (Scale s) = (Scale s :&# Zero, Zero :&# Scale s)
 unjoin2 (p :|# q) = (p,q)
-unjoin2 ((unjoin2 -> (p,q)) :&# (unjoin2 -> (r,s))) = (p :& r, q :& s)
+unjoin2 ((unjoin2 -> (p,q)) :&# (unjoin2 -> (r,s))) = (p :&# r, q :&# s)
 unjoin2 (ForkL ms) = (ForkL A.*** ForkL) (unzip (unjoin2 <$> ms))
 
 -- unjoin2 ((p :| q) :&# (r :| s)) = (p :&# r, q :#& s)
@@ -79,19 +79,21 @@ unjoin2 (ForkL ms) = (ForkL A.*** ForkL) (unzip (unjoin2 <$> ms))
 (ForkL *** ForkL) (unzip (unjoin <$> ms)) :: L f (k :.: h) s :* L g (k :.: h) s
 #endif
 
-unfork2 :: Additive s => L f (h :*: k) s -> L f h s :* L f k s
+unfork2 :: Semiring s => L f (h :*: k) s -> L f h s :* L f k s
 unfork2 Zero = (zero,zero)
+unfork2 (Scale s) = (Scale s .@ exl, Scale s .@ exr)
+                    -- (Scale s :|# Zero, Zero :|# Scale s)
 unfork2 (p :&# q) = (p,q)
 unfork2 ((unfork2 -> (p,q)) :|# (unfork2 -> (r,s))) = (p :|# r, q :|# s)
 unfork2 (JoinL ms) = (JoinL A.*** JoinL) (unzip (unfork2 <$> ms))
 
 -- unfork2 ((p :& q) :|# (r :& s)) = (p :|# r, q :|# s)
 
-pattern (:&) :: Additive s => L f h s -> L f k s -> L f (h :*: k) s
+pattern (:&) :: Semiring s => L f h s -> L f k s -> L f (h :*: k) s
 pattern u :& v <- (unfork2 -> (u,v)) where (:&) = (:&#)
 {-# complete (:&) #-}
 
-pattern (:|) :: Additive s => L f h s -> L g h s -> L (f :*: g) h s
+pattern (:|) :: Semiring s => L f h s -> L g h s -> L (f :*: g) h s
 pattern u :| v <- (unjoin2 -> (u,v)) where (:|) = (:|#)
 {-# complete (:|) #-}
 
@@ -99,8 +101,9 @@ pattern u :| v <- (unjoin2 -> (u,v)) where (:|) = (:|#)
 -- pattern Join ms <- (unjoinL -> ms) where Join = JoinL
 -- {-# complete Join #-}
 
-unforkL :: Representable h => L f (h :.: g) s -> h (L f g s)
+unforkL :: (Representable h, Semiring s) => L f (h :.: g) s -> h (L f g s)
 unforkL Zero       = pureRep Zero
+unforkL (Scale s)  = (Scale s .@) <$> exs
 unforkL (p :|# q)  = liftR2 (:|#) (unforkL p) (unforkL q)
 unforkL (ForkL ms) = ms
 unforkL (JoinL ms) = JoinL <$> distribute (unforkL <$> ms)
@@ -124,29 +127,62 @@ liftR2 (:|#) (unforkL p) (unforkL p') :: h (L (f :*: f') g s)
 JoinL <$> distrib (unforkL <$> ms) :: h (L (k :.: f) g s)
 #endif
 
-unjoinL :: Representable h => L (h :.: f) g s -> h (L f g s)
+unjoinL :: (Representable h, Semiring s) => L (h :.: f) g s -> h (L f g s)
 unjoinL Zero       = pureRep Zero
+unjoinL (Scale s)  = (Scale s .@) <$> ins
 unjoinL (p :&# p') = liftR2 (:&#) (unjoinL p) (unjoinL p')
 unjoinL (JoinL ms) = ms
 unjoinL (ForkL ms) = fmap ForkL (distribute (fmap unjoinL ms))
 
-pattern Fork :: V h => h (L f g s) -> L f (h :.: g) s
+pattern Fork :: (V h, Semiring s) => h (L f g s) -> L f (h :.: g) s
 pattern Fork ms <- (unforkL -> ms) where Fork = ForkL
 {-# complete Fork #-}
 
-pattern Join :: V h => h (L f g s) -> L (h :.: f) g s
+pattern Join :: (V h, Semiring s) => h (L f g s) -> L (h :.: f) g s
 pattern Join ms <- (unjoinL -> ms) where Join = JoinL
 {-# complete Join #-}
 
-instance Additive s => Additive (L f g s) where
+instance Semiring s => Additive (L f g s) where
   zero = Zero
   Zero + m = m
   m + Zero = m
   Scale s + Scale s' = Scale (s + s') -- distributivity
+  Scale s + m' = m' + Scale s  -- reshape Scale s to match m' 
   (f :|# g) + (h :| k) = (f + h) :| (g + k)
   (f :&# g) + (h :& k) = (f + h) :& (g + k)
   ForkL ms  + Fork ms' = Fork (ms +^ ms')
   JoinL ms  + Join ms' = Join (ms +^ ms')
+
+#if 1
+
+idL :: Semiring s => L f f s
+idL = Scale one
+
+-- Injections
+inl :: Semiring s => L f (f :*: g) s 
+inl = idL :& zero
+
+inr :: Semiring s => L g (f :*: g) s 
+inr = zero :& idL
+
+-- Projections
+exl :: Semiring s => L (f :*: g) f s 
+exl = idL :| zero
+
+exr :: Semiring s => L (f :*: g) g s 
+exr = zero :| idL
+
+-- Unjoining idL yields injections, while unforking gives projections:
+
+-- Injections
+ins :: (Representable h, Semiring s) => h (L f (h :.: f) s)
+ins = unjoinL idL
+
+-- Projections
+exs :: (Representable h, Semiring s) => h (L (h :.: f) f s)
+exs = unforkL idL
+
+#else
 
 -- | Row-major "matrix" of linear maps
 rowMajor' :: (V2 f g, V2 h k) => k (h (L f g s)) -> L (h :.: f) (k :.: g) s
@@ -166,29 +202,8 @@ idL = rowMajor (diagR (pureRep one))
 -- The (:.: Par1) requirement in  bothers me!
 -- Note that f :.: Par1 =~ f (just as i :* () =~ i, in logarithm land).
 
--- Note: unjoining idL yields injections, while unforking gives projections.
-
 -- TODO: simplify/restrict diagR & idL as Dan suggested
 -- (https://github.com/conal/linalg/pull/11#discussion_r460316931).
-
-infixr 9 .@
-(.@) :: Semiring s => L g h s -> L f g s -> L f h s
-Zero      .@ _         = Zero                      -- Zero denotation
-_         .@ Zero      = Zero                      -- linearity
-Scale a   .@ Scale b   = Scale (a * b)             -- Scale denotation
-(p :&# q) .@ m         = (p .@ m) :&# (q .@ m)     -- binary product law
-m         .@ (p :|# q) = (m .@ p) :|# (m .@ q)     -- binary coproduct law
-(r :|# s) .@ (p :&# q) = (r .@ p) + (s .@ q)       -- biproduct law
-ForkL ms' .@ m         = ForkL ((.@ m) <$> ms')    -- n-ary product law
-m'        .@ JoinL ms  = JoinL ((m' .@) <$> ms)    -- n-ary coproduct law
-JoinL ms' .@ ForkL ms  = sum (liftR2 (.@) ms' ms)  -- biproduct law
-
-instance (V f, Semiring s) => Semiring (L (f :.: Par1) (f :.: Par1) s) where
-  one = idL
-  (*) = (.@)
-
--- Illegal nested constraint ‘Eq (Rep f)’
--- (Use UndecidableInstances to permit this)
 
 -- Projections
 exl :: (V f, Semiring s) => L ((f :.: Par1) :*: g) (f :.: Par1) s 
@@ -200,3 +215,30 @@ exr = zero :| idL
 -- infixr 3 ***
 -- (***) :: L f g s -> L h k s -> L (f :*: h) (g :*: k) s
 -- p *** q = (p .@ exl) :& (q .@ exr)
+
+#endif
+
+infixr 9 .@
+(.@) :: Semiring s => L g h s -> L f g s -> L f h s
+Zero      .@ _         = Zero                      -- Zero denotation
+_         .@ Zero      = Zero                      -- linearity
+Scale a   .@ Scale b   = Scale (a * b)             -- Scale denotation
+(p :&# q) .@ m         = (p .@ m) :&# (q .@ m)     -- binary product law
+m         .@ (p :|# q) = (m .@ p) :|# (m .@ q)     -- binary coproduct law
+-- (r :|# s) .@ (p :&# q) = (r .@ p) + (s .@ q)       -- biproduct law
+
+(r :|# s) .@ (unfork2 -> (p,q)) = (r .@ p) + (s .@ q)       -- biproduct law
+
+ForkL ms' .@ m         = ForkL ((.@ m) <$> ms')    -- n-ary product law
+m'        .@ JoinL ms  = JoinL ((m' .@) <$> ms)    -- n-ary coproduct law
+-- JoinL ms' .@ ForkL ms  = sum (liftR2 (.@) ms' ms)  -- biproduct law
+JoinL ms' .@ (unforkL -> ms)  = sum (liftR2 (.@) ms' ms)  -- biproduct law
+
+-- (unjoin2 -> (r,s)) .@ (p :&# q) = undefined
+
+instance (V f, Semiring s) => Semiring (L (f :.: Par1) (f :.: Par1) s) where
+  one = idL
+  (*) = (.@)
+
+-- Illegal nested constraint ‘Eq (Rep f)’
+-- (Use UndecidableInstances to permit this)
